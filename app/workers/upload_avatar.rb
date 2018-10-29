45358b0811cac4979b9ddc08e58809180254d4d0
Dmitriy <tds050680@gmail.com>
2018-10-29 15:23:25 +0200

    Fixed comments on gitlab Ava cloudinary

--- app/workers/upload_avatar.rb
+++ app/workers/upload_avatar.rb
@@ -2,7 +2,7 @@ class UploadAvatar
   include Sidekiq::Worker
 
   def perform(path, user_id)
-    public_id = Cloudinary::Uploader.upload( path )['public_id']
+    public_id = Cloudinary::Uploader.upload( path, :folder => "dock/avatars/" )['public_id']
     user = User.find_by(id: user_id)
     user.avatar = public_id
     user.save
